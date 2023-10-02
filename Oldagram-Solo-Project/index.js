589ae6db5b7e660de42cfc7dc17c84f1afb66519
Jonserafim7 <142022025+Jonserafim7@users.noreply.github.com>
2023-10-02 20:40:12 -0300

    version 1.0

--- Oldagram-Solo-Project/index.js
+++ Oldagram-Solo-Project/index.js
@@ -1,3 +1,19 @@
+//Database
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://oldagram-296fa-default-rtdb.firebaseio.com/"
+}
+
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const postsInDb = ref(database, "oldgramPosts")
+
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -27,3 +43,82 @@ const posts = [
         likes: 152
     }
 ]
+
+
+
+const containerEl = document.querySelector("body")
+const btnEl = document.querySelector(".btn-test")
+
+
+function publishPost() {
+
+    containerEl.innerHTML = ""
+
+    createHeader()
+
+    for (let i = 0; i < posts.length; i++) {
+        let currentPost = posts[i];
+        createPost(currentPost)
+    }
+}
+
+
+
+
+function createHeader(array) {
+    
+    let header = `
+        <header>
+            <div class="container" class="header-container">
+                <img class="logo" src="images/logo.png" alt="logo image">
+                <img class="header-profile-picture" src="images/user-avatar.png" alt="profile picture">
+            </div>
+        </header>
+    `
+    containerEl.innerHTML += header
+}
+
+function createPost(array) {
+    let newPost = `
+    <div class="main-container">
+
+        <section class="user-info">
+            <div class="container">
+                <img class="avatar-img" src="${array.avatar}">
+                <div>
+                    <p class="bold-font">${array.name}</p>
+                    <p>${array.location}</p>
+                </div>
+            <div>
+        </section>
+
+        <section class="post-img-section">
+            <div class="container">
+                <img class="post-img" src="${array.post}">
+            </div>
+        </section>
+
+        <section class="footer">
+            <div class="container">
+                <div>
+                    <button id="like-btn"></button>
+                    <button id="comment-btn"></button>
+                    <button id="dm-button"></button>
+                </div>
+                <p class="bold-font">${array.likes} likes</p>
+                <p><span class="bold-font">${array.username}</span> ${array.comment}</p>
+            </div>
+        </section>
+    </div>
+
+    
+    `
+    containerEl.innerHTML += newPost
+}
+
+
+
+
+btnEl.addEventListener("click", function() {
+    publishPost()
+});
